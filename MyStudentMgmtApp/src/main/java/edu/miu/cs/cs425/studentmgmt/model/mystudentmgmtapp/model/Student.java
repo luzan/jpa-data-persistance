6bc21e43170e7d978ed9f79caa37693c9184dc06
Luzan Baral <luzanbaral@gmail.com>
2022-07-05 19:18:49 -0500

    Student Entity added

--- MyStudentMgmtApp/src/main/java/edu/miu/cs/cs425/studentmgmt/model/mystudentmgmtapp/model/Student.java
+++ MyStudentMgmtApp/src/main/java/edu/miu/cs/cs425/studentmgmt/model/mystudentmgmtapp/model/Student.java
@@ -1,4 +1,98 @@
 package edu.miu.cs.cs425.studentmgmt.model.mystudentmgmtapp.model;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studentId;
+
+    @Column(nullable = false)
+    private String studentNumber;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = true)
+    private String middleName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = true)
+    private double cgpa;
+
+    private LocalDate dateOfEnrollment;
+
+    public Student() {
+    }
+
+    public Student(String studentNumber, String firstName, String middleName, String lastName, double cgpa, LocalDate dateOfEnrollment) {
+        this.studentNumber = studentNumber;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.cgpa = cgpa;
+        this.dateOfEnrollment = dateOfEnrollment;
+    }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    public LocalDate getDateOfEnrollment() {
+        return dateOfEnrollment;
+    }
+
+    public void setDateOfEnrollment(LocalDate dateOfEnrollment) {
+        this.dateOfEnrollment = dateOfEnrollment;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", studentNumber='" + studentNumber + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", cgpa=" + cgpa +
+                ", dateOfEnrollment=" + dateOfEnrollment +
+                '}';
+    }
+
 }
